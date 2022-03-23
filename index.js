16bea559a5a9cad9782280a945eae6beb347a7b9
jorneylm <jorney95@live.com>
2022-03-22 19:42:20 -0600

    set  styles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 const router = require('./routes');
 
@@ -7,6 +8,7 @@ require('dotenv').config({ path: 'variables.env' });
 const app = express();
 
 //habilitar EJS como template engine
+app.use(expressLayouts)
 app.set('view engine', 'ejs');
 
 //ubicacion vistas
