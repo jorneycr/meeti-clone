cb3337f43c59d1ddfe5656f78e53cb393a9452e9
jorneylm <jorney95@live.com>
2022-03-22 18:43:54 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const router = require('./routes');
+
+require('dotenv').config({ path: 'variables.env' });
+
+const app = express();
+
+//habilitar EJS como template engine
+app.set('view engine', 'ejs');
+
+//ubicacion vistas
+app.set('views', path.join(__dirname,'./views'));
+
+//routing
+app.use('/', router());
+
+app.listen(process.env.PORT, () => {
+    console.log("Servidor funcionado");
+})
