1656da9ecb9422d4a6402869bfdf106abaaa8b52
jorneylm <jorney95@live.com>
2022-04-16 22:21:36 -0600

    changes

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,20 +1,21 @@
 const Usuarios = require('../models/Usuarios');
 
 exports.formCrearCuenta = (req, res) => {
-    res.render('crear-cuenta',{
-        nombrePagina:  'Crear tu  Cuenta'
+    res.render('crear-cuenta', {
+        nombrePagina: 'Crear tu  Cuenta'
     })
 }
 
-exports.crearNuevaCuenta = async (req, res)  => {
+exports.crearNuevaCuenta = async (req, res) => {
     const usuario = req.body;
-    const nuevoUsuario  = await Usuarios.create(usuario);
+    const nuevoUsuario = await Usuarios.create(usuario);
 
     try {
         //TODO : Flash Message y redireccionar
-    console.log(nuevoUsuario);
+        console.log(nuevoUsuario);
     } catch (error) {
-        console.log(error.errors);
+        const erroresSequelize = error.errors.map(err => err.message);
+        console.log(erroresSequelize);
     }
-    
+
 }
